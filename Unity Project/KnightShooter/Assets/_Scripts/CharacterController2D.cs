b8e75520d74d9bc3d6e07dee73d6de2d2aee394a
sean6258 <srlmysticmail@gmail.com>
2017-10-19 13:35:24 -0600

    Powerup Code
    Shotgun and Rocket are now functional and ingame

--- Unity Project/KnightShooter/Assets/_Scripts/CharacterController2D.cs
+++ Unity Project/KnightShooter/Assets/_Scripts/CharacterController2D.cs
@@ -13,7 +13,6 @@ public class CharacterController2D : MonoBehaviour {
 
     public UnityEvent levelEndEvent;
     public Rigidbody2D _shot;
-	public Rigidbody2D _shotgun;
 	public Rigidbody2D _rocket;
 	public int ammo;
 	public bool shield = false;
@@ -52,6 +51,7 @@ public class CharacterController2D : MonoBehaviour {
             _horizontalVelocity *= _airControl;
         }
 			
+			
 
 		//use button 0 for pc
 		//use button 16 for mac
@@ -88,8 +88,9 @@ public class CharacterController2D : MonoBehaviour {
 		//use button 18 for mac
         if (Input.GetKeyDown(KeyCode.Joystick1Button2))
         {
-			if (shotType == "Default") 
+			if (shotType == "Default" || ammo == 0) 
 			{
+				shotType = "Default";
 				Rigidbody2D bullet = Instantiate (_shot, transform.position, transform.rotation) as Rigidbody2D;
                 if (_verticalAim == 1)
                 {
@@ -107,26 +108,36 @@ public class CharacterController2D : MonoBehaviour {
 				}
 			}
 
-			if (shotType == "Shotgun") 
+			if (shotType == "Shotgun" && ammo != 0) 
 			{
-				Rigidbody2D bullet = Instantiate (_shotgun, transform.position, transform.rotation) as Rigidbody2D;
+				Rigidbody2D bullet = Instantiate (_shot, transform.position, transform.rotation) as Rigidbody2D;
+				Rigidbody2D bullet2 = Instantiate (_shot, transform.position, transform.rotation) as Rigidbody2D;
+				Rigidbody2D bullet3 = Instantiate (_shot, transform.position, transform.rotation) as Rigidbody2D;
                 if (_verticalAim == 1)
                 {
                     bullet.AddForce(new Vector2(0f, _shotSpeed));
+					bullet2.AddForce(new Vector2(1f, _shotSpeed));
+					bullet3.AddForce(new Vector2(-1f, _shotSpeed));
                 }
                 else if (_verticalAim == -1)
                 {
                     bullet.AddForce(new Vector2(0f, _shotSpeed * -1));
+					bullet2.AddForce(new Vector2(1f, _shotSpeed * -1));
+					bullet3.AddForce(new Vector2(-1f, _shotSpeed * -1));
                 }
                 else if (_right) {
 					bullet.AddForce (new Vector2 (_shotSpeed, _shotSpeed * _verticalAim));
+					bullet2.AddForce (new Vector2 (_shotSpeed, _shotSpeed * _verticalAim +1));
+					bullet3.AddForce (new Vector2 (_shotSpeed, _shotSpeed * _verticalAim -1));
 				}
 				else if (_left) {
 					bullet.AddForce (new Vector2 (_shotSpeed * -1, _shotSpeed * _verticalAim));
+					bullet2.AddForce (new Vector2 (_shotSpeed * -1, _shotSpeed * _verticalAim +1));
+					bullet3.AddForce (new Vector2 (_shotSpeed * -1, _shotSpeed * _verticalAim -1));
 				}
 			}
 
-			if (shotType == "Rocket") 
+			if (shotType == "Rocket" && ammo != 0) 
 			{
 				Rigidbody2D bullet = Instantiate (_rocket, transform.position, transform.rotation) as Rigidbody2D;
                 if (_verticalAim == 1)
@@ -156,5 +167,27 @@ public class CharacterController2D : MonoBehaviour {
                 _grounded = true;
             }
         }
+
+		if (collision.gameObject.tag == "Powerup") 
+		{
+			//check for powerup
+			if (gameObject.name == "shotgunPower") 
+			{
+				shotType = "Shotgun";
+				ammo = 12;
+			}
+
+			if (gameObject.name == "rocketPower") 
+			{
+				shotType = "Rocket";
+				ammo = 2;
+			}
+
+			if (gameObject.name == "shieldPower") 
+			{
+				//durability + 1
+			}
+		}
+			
     }
 }
---
+++ Unity Project/KnightShooter/Assets/_Scripts/RocketController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RocketController : MonoBehaviour {
+
+	Collider2D[] explodeRadius;
+	// Use this for initialization
+	void Start () {
+		explodeRadius = new Collider2D[20];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+		if ((collision.gameObject.tag == "Player") || (collision.gameObject.tag == "DestructiblePlat" || (collision.gameObject.tag == "IndestructiblePlat")))
+        {
+			Physics2D.OverlapCircle(transform.position, 1.5f, new ContactFilter2D(), explodeRadius);
+
+			foreach (Collider2D o in explodeRadius) {
+				if ((o.gameObject.tag == "Player") || (collision.gameObject.tag == "DestructiblePlat")) 
+				{
+					Destroy (o.gameObject);	
+				}
+			}
+			Destroy(gameObject);
+        }
+    }
+}
